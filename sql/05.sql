f74de29461bdc29c1130030d6f07d31c914d2016
Viktor Zamaruev <vzamaruev@gmail.com>
2013-07-06 11:18:54 +0300

    Fix query

--- sql/05.sql
+++ sql/05.sql
@@ -1,2 +1,2 @@
 -- drop duplicata entries on user_profile table
-ALTER IGNORE T ALTER IGNORE TABLE user_profile ADD UNIQUE INDEX idx_user_name (user_id, name ); 
+ALTER IGNORE TABLE user_profile ADD UNIQUE INDEX idx_user_name (user_id, name); 
